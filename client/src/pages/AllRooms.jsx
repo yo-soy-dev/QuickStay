bb178939bd6fc44d888262143dfe6203f94117d1
Devansh Tiwari <devanshtiwari817@gmail.com>
2025-11-29 09:28:05 +0530

    Update AllRooms.jsx

--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -79,26 +79,26 @@ const AllRooms = () => {
     return room.hotel.city.toLowerCase().includes(destination.toLowerCase());
   };
 
-  // const filteredRooms = useMemo(() => {
-  //   return rooms
-  //     .filter((room) => matchesRoomType(room) && matchesPriceRange(room) && filterDestination(room))
-  //     .sort(sortRooms);
-  // }, [rooms, selectedFilters, selectedSort, searchParams]);
+  const filteredRooms = useMemo(() => {
+    return rooms
+      .filter((room) => matchesRoomType(room) && matchesPriceRange(room) && filterDestination(room))
+      .sort(sortRooms);
+  }, [rooms, selectedFilters, selectedSort, searchParams]);
 
- const filteredRooms = useMemo(() => {
-  let list = rooms.filter(room => 
-    matchesRoomType(room) && matchesPriceRange(room)
-  );
+//  const filteredRooms = useMemo(() => {
+//   let list = rooms.filter(room => 
+//     matchesRoomType(room) && matchesPriceRange(room)
+//   );
 
-  // apply destination filter strictly
-  const destination = searchParams.get("destination");
-  if (destination) {
-    list = list.filter(room => filterDestination(room));
-  }
+//   // apply destination filter strictly
+//   const destination = searchParams.get("destination");
+//   if (destination) {
+//     list = list.filter(room => filterDestination(room));
+//   }
 
-  // apply sorting
-  return list.sort(sortRooms);
-}, [rooms, selectedFilters, selectedSort, searchParams]);
+//   // apply sorting
+//   return list.sort(sortRooms);
+// }, [rooms, selectedFilters, selectedSort, searchParams]);
 
 
 
