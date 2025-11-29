8b7e78bb7d2a46f9aa8ef0400a06394fa279ccfd
Devansh Tiwari <devanshtiwari817@gmail.com>
2025-11-29 08:29:10 +0530

    Update Room.js

--- server/models/Room.js
+++ server/models/Room.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const roomSchema = new mongoose.Schema({
-  hotel: { type: String, ref: "Hotel", required: true },
+  hotel: { type: mongoose.Schema.Types.ObjectId, ref: "Hotel", required: true },
   roomType: { type: String, required: true },
   pricePerNight: { type: Number, required: true },
   amenities: { type: Array, required: true },
