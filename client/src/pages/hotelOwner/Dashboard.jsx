5fd7b7249d7edfeb07841e2c2e4bd717d4126d34
Devansh Tiwari <devanshtiwari817@gmail.com>
2025-11-29 09:36:02 +0530

    Update Dashboard.jsx

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -46,28 +46,28 @@ const Dashboard = () => {
       />
 
       <div className="flex gap-4 my-8">
-        <div className="bg-primary/3 border border-primary/10 rounded flex p-4 pr-8">
+        <div className="bg-blue-600/3 border border-primary/10 rounded flex p-4 pr-8">
           <img
             src={assets.totalBookingIcon}
             alt=""
             className="max-sm:hidden h-10"
           />
           <div className="flex flex-col sm:ml-4 font-medium">
-            <p className="text-blue-500 text-lg">Total Bookings</p>
+            <p className="text-blue-600 text-lg">Total Bookings</p>
             <p className="text-neutral-400 text-base">
               {dashboardData.totalBookings}
             </p>
           </div>
         </div>
 
-        <div className="bg-primary/3 border border-primary/10 rounded flex p-4 pr-8">
+        <div className="bg-blue-600/3 border border-blue-600/10 rounded flex p-4 pr-8">
           <img
             src={assets.totalRevenueIcon}
             alt=""
             className="max-sm:hidden h-10"
           />
           <div className="flex flex-col sm:ml-4 font-medium">
-            <p className="text-blue-500 text-lg">Total Revenue</p>
+            <p className="text-blue-600 text-lg">Total Revenue</p>
             <p className="text-neutral-400 text-base">
               {currency} {dashboardData.totalRevenue}
             </p>
