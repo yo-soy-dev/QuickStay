a4d091933dcbe630982a4c9a53ca9f27f2b962d9
Devansh Tiwari <devanshtiwari817@gmail.com>
2025-11-29 09:37:23 +0530

    Update Loader.jsx

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -16,7 +16,7 @@ const Loader = () => {
 
   return (
     <div className='flex justify-center items-center h-screen'>
-      <div className='animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-primary'></div>
+      <div className='animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-blue-600'></div>
     </div>
   );
 };
